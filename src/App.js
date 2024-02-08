3173292a15d6fe22d76baf22e152a116fe7f120a
Feline Huhn <feline.huhn@gmx.de>
2024-02-08 14:40:00 +0100

    resolve challenge

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from "react";
 import Counter from "./components/Counter";
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [people, setPeople] = useState(0);
+
+  function handleAddPeople() {
+    setPeople(people + 1);
+  }
+
+  function handleDeletePeople() {
+    if (people === 0) return;
+    setPeople(people - 1);
+  }
+
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      <Counter
+        onAddPeople={handleAddPeople}
+        onDeletePeople={handleDeletePeople}
+      />
+      <p>You are going to reserve a table for {people} people.</p>
     </div>
   );
 }
